d11369f4dccc8957722fda0fd9880cb1ae738d25
Aditya4095 <ad963939@dal.ca>
2024-05-26 22:14:39 -0300

    lab1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
+  const [visits, setVisits] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            {/* Pass incrementVisits function to Home component */}
+            <Route path="/" element={<Home bannerId="B00896049 " visits={visits} setVisits={setVisits} />} />
+            {/* Pass visits prop directly to About and Contact components */}
+            <Route path="/about" element={<About visits={visits} />} />
+            <Route path="/contact" element={<Contact visits={visits} />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+// Home.js
+import React, { useEffect } from 'react';
+
+const Home = ({ bannerId, visits, setVisits }) => {
+
+  useEffect(() => {
+    setVisits(visits++);
+  }, [setVisits, visits]);
+
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>Home page visited {visits} times</p>
+      <p>Display your Banner Id: {bannerId}</p>
+    </div>
+  );
+};
+
+export default Home;
